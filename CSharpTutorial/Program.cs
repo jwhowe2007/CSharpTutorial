1fbcd1b99f09c0ad44b84084890196c32829e71b
Justin Howe <57464428+jwhowe2007@users.noreply.github.com>
2020-03-24 14:32:54 -0600

    Add the tutorial class with some XML docblock comments and normal comments

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,7 +6,15 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            // Single line comment
+
+            /* 
+             * Comment line 1
+             * Comment line 2
+             */
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Square of 32: {0}", Basics.Tutorial.Square(32));
         }
     }
 }
---
+++ CSharpTutorial/Sandbox/Tutorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basics
+{
+    class Tutorial
+    {
+        /// <summary>
+        /// This is an XML DocBlock that provides more information and context about a
+        /// chunk of code for easier readability and may be used to extend documentation
+        /// procedures within a given codebase.
+        /// </summary>
+        /// <param name="x">Integer to be squared</param>
+        /// <returns>Square of the given number (x^2)</returns>
+        public static Int64 Square(Int64 x)
+        {
+            return x * x;
+        }
+    }
+}
