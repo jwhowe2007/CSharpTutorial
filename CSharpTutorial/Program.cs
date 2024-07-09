ccac9bf880daafbbee30c8959ce3b8d5c8c1a469
Justin Howe <jwhowe02@proton.me>
2024-07-09 09:30:41 -0700

    Changed output a little, added an app icon and some more comments

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -12,11 +12,18 @@ namespace CSharpTutorial
              * Comment line 1
              * Comment line 2
              */
-            Console.WriteLine("Hello World!");
 
+            ///
+            /// Heredoc-style comment
+            /// Everything in these blocks is treated as one long literal
+            /// broken up into multiple lines, and functions much like the delimited block style shown above.
+            /// This, however, can be used to specify special capabilities and flags for the compiler and any other tool that
+            /// needs to use this information as metadata.
+            /// 
+            Console.WriteLine("C# Intro Program");
             Console.WriteLine("Square of 8192: {0}", Basics.Tutorial.Square(8192));
-            Basics.Tutorial tutorial = new Basics.Tutorial();
 
+            Basics.Tutorial tutorial = new();
             Basics.Tutorial.BasicDataTypes();
             tutorial.NumbersFun();
         }
