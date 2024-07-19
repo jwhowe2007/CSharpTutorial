58dfc039d488218cc7c9ee057c60f61354c8ddfa
Justin Howe <jwhowe02@proton.me>
2024-07-19 10:47:04 -0700

    Improve theme toggle button behavior

--- WebAppTutorial/app.js
+++ WebAppTutorial/app.js
@@ -1,2 +1,15 @@
+'use strict'; // Let's be strict about our JS!
+
 // Main index JS
-console.log("JavaScript in use!");
+const themeSwitcher = document.querySelector('#themeSwitch');
+
+themeSwitcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+    if(document.body.className == 'light-theme') {
+        this.textContent = "Use Dark";
+    } else {
+        this.textContent = "Use Light";
+    }
+});
